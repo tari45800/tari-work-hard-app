92435f2385ea06b12744d204bb7abc63bc709058
tari45800 <tari45800@gmail.com>
2024-07-05 19:07:00 +0900

    feat: 텍스트 관리 기능 추가

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>투두앱</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { theme } from "./colors";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
+
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const work = () => {
+    setWorking(true);
+  };
+  const travel = () => {
+    setWorking(false);
+  };
+  const onChangeText = (payload) => setText(payload);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.gray }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.gray,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          value={text}
+          onChangeText={onChangeText}
+          style={styles.input}
+          placeholder={working ? "할 일을 추가하셔" : "어디로 가고 싶으셔"}
+        ></TextInput>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
